8f35761c9cabed016c96523788ca55bee1e46a60
Mohamed Irfan P <pabi.adn1@gmail.com>
2023-07-05 20:46:27 +0530

    [ADD] firebase configured for image upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+ 
+const firebaseConfiguration = {
+    apiKey: "AIzaSyCPj-Ag-j22qcSEoBuV5H0vvncfq7KuBGE",
+    authDomain: "franchiseconnect-d1f04.firebaseapp.com",
+    projectId: "franchiseconnect-d1f04",
+    storageBucket: "franchiseconnect-d1f04.appspot.com",
+    messagingSenderId: "1018175403429",
+    appId: "1:1018175403429:web:119b1491c385ee4b7a244c",
+    measurementId: "G-9SR191G4CX"
+  };
+
+// Initialize Firebase
+const app = initializeApp (firebaseConfiguration);
+ 
+// Firebase storage reference
+const storage = getStorage(app);
+export default storage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const animationUtilities = {
+        '.animate-gradient': {
+          animation: 'gradientAnimation 5s linear infinite',
+        },
+      };
+
+      const keyframes = {
+        gradientAnimation: {
+          '0%': {
+            'background-position': '0% 50%',
+          },
+          '100%': {
+            'background-position': '100% 50%',
+          },
+        },
+      };
+
+      addUtilities(animationUtilities, ['responsive']);
+      addUtilities(keyframes, ['responsive']);
+    },
+  ],
 }
 
