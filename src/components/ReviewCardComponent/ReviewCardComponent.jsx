affc1f38bad140ebc74ea84147064e2ba3c1de44
Mohamed Irfan P <pabi.adn1@gmail.com>
2023-07-06 22:51:09 +0530

    [Update] Ui fixes

--- src/components/ReviewCardComponent/ReviewCardComponent.jsx
+++ src/components/ReviewCardComponent/ReviewCardComponent.jsx
@@ -2,23 +2,22 @@ import {Card} from 'primereact/card'
 import React from 'react';
 import {Rating} from "primereact/rating";
 
-function ReviewCardComponent(props) {
-    const {review} = props;
+function ReviewCardComponent({review}) {
     const header = (
         <div className="flex gap-5 justify-start items-center">
             <span className="w-10 h-10 rounded-full overflow-hidden outline ml-4 mt-3">
                 <img src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png" alt="User Icon" className="w-full h-full object-cover"/>
             </span>
             <div>
-                <h3 className="font-medium mt-3">Anurag Srivastava</h3>
+                <h3 className="font-medium mt-3">{review.name}</h3>
             </div>
         </div>
 
     )
 
     const footer = (
-        <div className="card flex justify-content-center">
-            <Rating value={5}
+        <div className="card flex justify-start">
+            <Rating value={review.rating}
                 readOnly
                 cancel={false}/>
         </div>
@@ -31,7 +30,7 @@ function ReviewCardComponent(props) {
                 className='md:w-1/4 rounded-md'>
                 <hr className='border border-blue-300 -mt-8'></hr>
                 <div className='mt-2'>
-                    <p className='word-wrap'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem esse delectus distinctio sapiente corporis tenetur aperiam, saepe eius incidunt aut.</p>
+                    <p className='word-wrap'>{review.comment}</p>
                 </div>
             </Card>
         </>
