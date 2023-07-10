ad1c4fbb6e2782d05c6fedde912ed0ed76e8de9f
Mohamed Irfan P <pabi.adn1@gmail.com>
2023-07-10 22:06:56 +0530

    [Fix] Fixes Filter functionality

--- src/components/Filters/FilterComponent.jsx
+++ src/components/Filters/FilterComponent.jsx
@@ -1,92 +1,142 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 import RangeSelectorFilter from './RangeSelectorFilter.jsx';
 
 import ButtonComponent from '../ButtonComponent/ButtonComponent.jsx';
 
-import {MultiSelect} from 'primereact/multiselect';
+import { MultiSelect } from 'primereact/multiselect';
 import { Checkbox } from 'primereact/checkbox';
 import { useDebounce } from 'primereact/hooks';
-    
+
 
 function getUniqueFranchiseIndustries(franchises) {
     const uniqueIndustries = new Map();
-  
-    franchises.forEach((franchise) => {
-      const industry = franchise.franchise.franchiseIndustry;
-      const code = `${industry[0]}${industry[1]}`;
-  
-      uniqueIndustries.set(code, { name: industry, code });
-    });
-  
-    return Array.from(uniqueIndustries.values());
-  }
-  
 
-const MainFilterComponent = ({franchises}) => {
+    franchises.forEach((franchise) => {
+        const industry = franchise.franchise.franchiseIndustry;
+        const code = `${industry[0]}${industry[1]}`;
+
+        uniqueIndustries.set(code, { name: industry, code });
+    });
+
+    return Array.from(uniqueIndustries.values());
+}
+
+
+const MainFilterComponent = ({ franchises, setCopyFranchises }) => {
     const [selectedCategories, debouncedValue, setSelectedCategories] = useDebounce([], 400);
     // const [selectedCategories, setSelectedCategories] = useState(null);
-    const [checkboxData, setCheckboxData] = useState({});
-    const [rangeSelectorData, setRangeSelectorData] = useState({minValue: 0, maxValue: 100});
+    const [checkboxData, setCheckboxData] = useState(false);
+    const [rangeInvestmentSelectorData, setRangeInvestmentSelectorData] = useState({ minValue: 0, maxValue: 10000000 });
+    const [rangeSpaceSelectorData, setRangeSpaceSelectorData] = useState({ minValue: 200, maxValue: 100000 });
 
     const franchiseCategories = getUniqueFranchiseIndustries(franchises);
 
 
-    const handleRangeSelectorChange = (minValue, maxValue) => {
-        setRangeSelectorData({minValue, maxValue});
+    const handleRangeInvestmentSelectorChange = (Value) => {
+        setRangeInvestmentSelectorData(Value);
     };
 
-    const handleFilterApply = () => {
-        console.log("FILTER API")
+    const handleRangeSpaceSelectorChange = (Value) => {
+        setRangeSpaceSelectorData(Value);
     }
 
+    const handleFilterApply = () => {
+        console.log(checkboxData)
+        console.log(selectedCategories)
+
+        const filteredFranchises = franchises.filter((franchise) => {
+            const {
+                franchiseInvestment,
+                franchiseSpace,
+                franchiseCustomizedOption,
+            } = franchise.franchise;
+
+            // Filter based on selectedCategories
+            if (
+                selectedCategories.length > 0 &&
+                !selectedCategories.some((category) => category.name === franchise.franchise.franchiseIndustry)
+            ) {
+                return false;
+            }
+
+            // Filter based on rangeInvestmentSelectorData
+            if (
+                franchiseInvestment < rangeInvestmentSelectorData.minValue ||
+                franchiseInvestment > rangeInvestmentSelectorData.maxValue
+            ) {
+                return false;
+            }
+
+            // Filter based on rangeSpaceSelectorData
+            if (
+                franchiseSpace < rangeSpaceSelectorData.minValue ||
+                franchiseSpace > rangeSpaceSelectorData.maxValue
+            ) {
+                return false;
+            }
+
+            // Filter based on checkboxData
+            if (checkboxData && !franchiseCustomizedOption) {
+                return false;
+            }
+
+            return true;
+        });
+
+        setCopyFranchises( filteredFranchises
+          );
+    };
+
+
     return (
-        <div className='p-5 sticky top-0 z-10'>
+        <div className='p-5 sticky top-0 z-10 h-full'>
             <section className='border border-[#6366F1] bg-blue-100 rounded p-3 pb-8'>
 
-            <h6 className='text-center font-medium'>Filter Options</h6>
-            <div className="form-group flex items-center gap-1 p-5   rounded mt-2">
-                <label>Categories:</label>
-                <MultiSelect value={selectedCategories}
-                    onChange={
-                        (e) => {
-                            setSelectedCategories(e.value)}
-                    }
-                    options={franchiseCategories}
-                    optionLabel="name"
-                    filter
-                    placeholder="Select Category"
-                    maxSelectedLabels={1}
-                    className=""/>
-            </div>
+                <h6 className='text-center font-medium'>Filter Options</h6>
+                <div className="form-group flex items-center gap-1 p-5  flex-col lg:flex-row rounded mt-2">
+                    <label>Categories:</label>
+                    <MultiSelect value={selectedCategories}
+                        onChange={
+                            (e) => {
+                                setSelectedCategories(e.value)
+                            }
+                        }
+                        options={franchiseCategories}
+                        optionLabel="name"
+                        filter
+                        placeholder="Select Category"
+                        maxSelectedLabels={1}
+                        className="" />
+                </div>
 
-            <div className="form-group flex items-center gap-1 p-5   rounded mt-2">
-                <label>Investment (INR):</label>
-                <RangeSelectorFilter min={0}
-                    max={1000000}
-                    onChange={handleRangeSelectorChange}
-                    className="inline-block relative"
+                <div className="form-group flex items-center gap-1 p-5   rounded mt-2 flex-col lg:flex-row ">
+                    <label>Investment (INR):</label>
+                    <RangeSelectorFilter min={0}
+                        max={1000000}
+                        onChange={handleRangeInvestmentSelectorChange}
+                        className="inline-block relative"
                     />
-            </div>
-            <div className='form-group flex items-center gap-1 p-5  rounded mt-2'>
-              <label>Space (sq. ft):</label>
-              <RangeSelectorFilter min={200}
-                    max={1000000}
-                    onChange={handleRangeSelectorChange}/>
-            </div>
-            <div className='form-group flex items-center gap-1 p-5   rounded mt-2'>
-                <Checkbox onChange={e => setCheckboxData(e.checked)} checked={checkboxData} inputId='customize'></Checkbox>
-                <label htmlFor="customize">Customization Option Allowed Only</label>
-                
-            </div>
+                </div>
+                <div className='form-group flex items-center gap-1 p-5 mt-5 lg:mt-2 rounded flex-col lg:flex-row '>
+                    <label>Space (sq. ft):</label>
+                    <RangeSelectorFilter min={200}
+                        max={100000}
+                        onChange={handleRangeSpaceSelectorChange} />
+                </div>
+                <div className='form-group flex items-center gap-1 p-5   rounded mt-5 lg:mt-2 '>
+                    <Checkbox onChange={e => setCheckboxData(e.checked)} checked={checkboxData} inputId='customize'></Checkbox>
+                    <label htmlFor="customize">Customization Option Allowed Only</label>
 
-            <div className='text-center'>
-                <ButtonComponent text={"Apply Filter"} onClick={()=>{
-                    console.log("filter applied")
-                }}/>
-            </div>
-                </section>
+                </div>
+
+                <div className='text-center flex-col'>
+                    <ButtonComponent text={"Apply Filter"} onClick={() => {
+                        handleFilterApply();
+                    }} />
+                </div>
+            </section>
         </div>
     );
 };
--- src/components/Filters/RangeSelectorFilter.jsx
+++ src/components/Filters/RangeSelectorFilter.jsx
@@ -37,9 +37,9 @@ const MultiRangeSlider = ({ min, max, onChange, className}) => {
   }, [maxVal, getPercent]);
 
   // Get min and max values when their state changes
-  // useEffect(() => {
-  //   onChange({ min: minVal, max: maxVal });
-  // }, [minVal, maxVal, onChange]);
+  useEffect(() => {
+    onChange({ min: minVal, max: maxVal });
+  }, [minVal, maxVal]);
 
   function setExtremes(setState,value, isMin)
   {
